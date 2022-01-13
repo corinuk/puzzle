6cf011a450fa0d9edccad3d0ef945da88eb68a31
Seong-uk-An <asu970602@naver.com>
2022-01-13 14:51:01 +0900

    fix firebase invalid api key err

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -26,9 +26,7 @@ function ReservationForm() {
     const phoneNum = parentNode[0].value;
 
     const data = `${year}년${month}월${day}일 ${hour}:${minute}:${second}\n${menu}\n${place}\n${address}\n${prevPrice}\n${saledPrice}\n${deadline}\n${pickupTime}\n${phoneNum}`;
-    const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
-    const url = `${PROXY}${process.env.REACT_APP_SLACK_WEBHOOK_URL}`;
-    // const url = process.env.REACT_APP_SLACK_WEBHOOK_URL;
+    const url = process.env.REACT_APP_SLACK_WEBHOOK_URL;
     try {
       const result = await axios({
         method: "post",
