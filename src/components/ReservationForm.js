eb9e8efdef661dae4c027240ad7cbedfb37b0498
Seong-uk-An <asu970602@naver.com>
2022-01-14 00:48:17 +0900

    fix 404 err

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -11,12 +11,12 @@ function ReservationForm({
 }) {
   const { reservationForm, phoneNumClass, time, submitBtn } = styles;
 
-  const moveToSuccess = () => {
-    window.location.href = "https://frosty-neumann-cfddf9.netlify.app/success";
-  };
+  // const moveToSuccess = () => {
+  //   window.location.href = "https://frosty-neumann-cfddf9.netlify.app/success";
+  // };
   const onSubmit = (event) => {
     event.preventDefault();
-    window.onhashchange = moveToSuccess();
+    window.onhashchange = console.log("hi");
   };
 
   return (
