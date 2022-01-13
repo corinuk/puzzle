69cee953fe52b38aff5102b617815ae38e3818cc
Seong-uk-An <asu970602@naver.com>
2022-01-13 09:27:51 +0900

    fix 405 err

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,37 @@
 import { useEffect, useState } from "react";
 import AppRouter from "routes/Router";
-// import { authService, authStateChanged } from "fb";
+import { authService, authStateChanged } from "fb";
 
 function App() {
   // const currentUser = authService.currentUser;
   const [loading, setLoading] = useState(true);
-  // const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // useEffect(() => {
-  //   authStateChanged(
-  //     authService,
-  //     (user) => {
-  //       if (user) {
-  //         setIsLoggedIn(true);
-  //       } else {
-  //         setIsLoggedIn(false);
-  //       }
-  //       setLoading(false);
-  //     },
-  //     (err) => {
-  //       console.error(err);
-  //     },
-  //     (complete) => {
-  //       console.log(complete);
-  //     }
-  //   );
-  // }, []);
+  useEffect(() => {
+    authStateChanged(
+      authService,
+      (user) => {
+        if (user) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+        setLoading(false);
+      },
+      (err) => {
+        console.error(err);
+      },
+      (complete) => {
+        console.log(complete);
+      }
+    );
+  }, []);
 
   //로그인 없이 바로 홈화면 띄우기
-  const isLoggedIn = true;
-  useEffect(() => {
-    setLoading(false);
-  }, []);
+  // const isLoggedIn = true;
+  // useEffect(() => {
+  //   setLoading(false);
+  // }, []);
 
   return (
     <>
