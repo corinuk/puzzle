e676effb288184ddee5f6c77240f9bc6ee97c2dc
Seong-uk-An <asu970602@naver.com>
2022-01-13 15:41:59 +0900

    fix firebase invalid api key err

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -16,14 +16,14 @@ function ReservationForm() {
     const hour = present.getHours();
     const minute = present.getMinutes();
     const second = present.getSeconds();
-    const menu = parentNode.parentNode.children[2].innerHTML;
-    const place = parentNode.parentNode.children[3].innerHTML;
-    const address = parentNode.parentNode.children[4].innerHTML;
-    const prevPrice = parentNode.parentNode.children[5].innerHTML;
-    const saledPrice = parentNode.parentNode.children[6].innerHTML;
-    const deadline = parentNode.parentNode.children[7].innerHTML;
-    const pickupTime = parentNode[1].value;
-    const phoneNum = parentNode[0].value;
+    const menu = parentNode.parentNode.parentNode.children[2].innerHTML;
+    const place = parentNode.parentNode.parentNode.children[3].innerHTML;
+    const address = parentNode.parentNode.parentNode.children[4].innerHTML;
+    const prevPrice = parentNode.parentNode.parentNode.children[5].innerHTML;
+    const saledPrice = parentNode.parentNode.parentNode.children[6].innerHTML;
+    const deadline = parentNode.parentNode.parentNode.children[7].innerHTML;
+    const pickupTime = parentNode.parentNode[1].value;
+    const phoneNum = parentNode.parentNode[0].value;
 
     const data = `${year}년${month}월${day}일 ${hour}:${minute}:${second}\n${menu}\n${place}\n${address}\n${prevPrice}\n${saledPrice}\n${deadline}\n${pickupTime}\n${phoneNum}`;
     const url = process.env.REACT_APP_SLACK_WEBHOOK_URL;
@@ -57,29 +57,38 @@ function ReservationForm() {
       name="contact"
       className={reservationForm}
       onSubmit={onSubmit}
-      method="POST"
+      method="post"
     >
       <input type="hidden" name="form-name" value="contact" />
-      <label>휴대폰 번호를 입력해주세요 ( - 제외 )</label>
-      <input
-        required
-        id="phone-num"
-        type="number"
-        placeholder="휴대폰 번호"
-        className={phoneNumClass}
-      />
-      <label>픽업하실 시간을 입력해주세요</label>
-      <select id="time" className={time}>
-        <option>10분뒤</option>
-        <option>20분뒤</option>
-        <option>30분뒤</option>
-      </select>
-      <input
-        type="button"
-        className={submitBtn}
-        value="예약하기"
-        onClick={onSubmit}
-      />
+      <p>
+        <label htmlFor="phone">휴대폰 번호를 입력해주세요 ( - 제외 )</label>
+        <br />
+        <input
+          name="phone"
+          required
+          id="phone"
+          type="number"
+          placeholder="휴대폰 번호"
+          className={phoneNumClass}
+        />
+      </p>
+      <p>
+        <label htmlFor="time">픽업하실 시간을 입력해주세요</label>
+        <br />
+        <select name="time" id="time" className={time}>
+          <option>10분뒤</option>
+          <option>20분뒤</option>
+          <option>30분뒤</option>
+        </select>
+      </p>
+      <p>
+        <input
+          type="submit"
+          className={submitBtn}
+          value="예약하기"
+          onClick={onSubmit}
+        />
+      </p>
     </form>
   );
 }
