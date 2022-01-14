c0982d849fc30b7f0e9afde89b9b1ffa6a898d97
Seong-uk-An <asu970602@naver.com>
2022-01-14 17:00:21 +0900

    fix redirection err

--- src/routes/Food.js
+++ src/routes/Food.js
@@ -5,60 +5,52 @@ import ReservationForm from "components/ReservationForm";
 import CompleteReserv from "components/CompleteReserv";
 
 function Food() {
-  const location = useLocation();
-  const {
-    state,
-    state: { menu, place, address, prevPrice, saledPrice, deadline, fileURL },
-  } = location;
+  try {
+    const location = useLocation();
+    const {
+      state: { menu, place, address, prevPrice, saledPrice, deadline, fileURL },
+    } = location;
 
-  const {
-    foodDiv,
-    img,
-    detail,
-    menuClass,
-    placeClass,
-    addressClass,
-    prevPriceClass,
-    saledPriceClass,
-    deadlineClass,
-  } = styles;
-
-  return (
-    <div className={foodDiv}>
-      {(function () {
-        if (state) {
-          return (
-            <>
-              <GoToPrevPage />
-              <img className={`${img}`} alt="food-img" src={fileURL} />
-              <h3 className={`${detail} ${menuClass}`}>{menu}</h3>
-              <span className={`${detail} ${placeClass}`}>{place}</span>
-              <span className={`${detail} ${addressClass}`}>{address}</span>
-              <span className={`${detail} ${prevPriceClass}`}>
-                원가 : {Number(`${prevPrice}`).toLocaleString("en")}원
-              </span>
-              <span className={`${detail} ${saledPriceClass}`}>
-                할인가 : {Number(`${saledPrice}`).toLocaleString("en")}원
-              </span>
-              <span className={`${detail} ${deadlineClass}`}>
-                마감시간 : {deadline}
-              </span>
-              <ReservationForm
-                menu={menu}
-                place={place}
-                address={address}
-                prevPrice={prevPrice}
-                saledPrice={saledPrice}
-                deadline={deadline}
-              />
-            </>
-          );
-        } else {
-          return <CompleteReserv />;
-        }
-      })()}
-    </div>
-  );
+    const {
+      foodDiv,
+      img,
+      detail,
+      menuClass,
+      placeClass,
+      addressClass,
+      prevPriceClass,
+      saledPriceClass,
+      deadlineClass,
+    } = styles;
+    return (
+      <div className={foodDiv}>
+        <GoToPrevPage />
+        <img className={`${img}`} alt="food-img" src={fileURL} />
+        <h3 className={`${detail} ${menuClass}`}>{menu}</h3>
+        <span className={`${detail} ${placeClass}`}>{place}</span>
+        <span className={`${detail} ${addressClass}`}>{address}</span>
+        <span className={`${detail} ${prevPriceClass}`}>
+          원가 : {Number(`${prevPrice}`).toLocaleString("en")}원
+        </span>
+        <span className={`${detail} ${saledPriceClass}`}>
+          할인가 : {Number(`${saledPrice}`).toLocaleString("en")}원
+        </span>
+        <span className={`${detail} ${deadlineClass}`}>
+          마감시간 : {deadline}
+        </span>
+        <ReservationForm
+          menu={menu}
+          place={place}
+          address={address}
+          prevPrice={prevPrice}
+          saledPrice={saledPrice}
+          deadline={deadline}
+        />
+      </div>
+    );
+  } catch (err) {
+    return <CompleteReserv />;
+  }
 }
 
 export default Food;
