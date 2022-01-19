6dff77be07c33ee9a130938d3e5efbfa9f40414c
Seong-uk-An <asu970602@naver.com>
2022-01-19 18:46:49 +0900

    back

--- src/routes/Food.js
+++ src/routes/Food.js
@@ -6,50 +6,50 @@ import ReservationForm from "components/ReservationForm";
 // import PhoneAuth from "components/PhoneAuth";
 
 function Food() {
-  // try {
-  const location = useLocation();
-  const {
-    state: {
-      id,
-      menu,
-      place,
-      address,
-      prevPrice,
-      saledPrice,
-      deadline,
-      createdAt,
-      fileURL,
-    },
-  } = location;
+  try {
+    const location = useLocation();
+    const {
+      state: {
+        id,
+        menu,
+        place,
+        address,
+        prevPrice,
+        saledPrice,
+        deadline,
+        createdAt,
+        fileURL,
+      },
+    } = location;
 
-  const {
-    foodDiv,
-    img,
-    detail,
-    menuClass,
-    placeClass,
-    addressClass,
-    prevPriceClass,
-    saledPriceClass,
-    deadlineClass,
-  } = styles;
-  return (
-    <div className={foodDiv}>
-      <GoToPrevPage />
-      <img className={`${img}`} alt="food-img" src={fileURL} />
-      <h3 className={`${detail} ${menuClass}`}>{menu}</h3>
-      <span className={`${detail} ${placeClass}`}>{place}</span>
-      <span className={`${detail} ${addressClass}`}>{address}</span>
-      <span className={`${detail} ${prevPriceClass}`}>
-        원가 : {Number(`${prevPrice}`).toLocaleString("en")}원
-      </span>
-      <span className={`${detail} ${saledPriceClass}`}>
-        할인가 : {Number(`${saledPrice}`).toLocaleString("en")}원
-      </span>
-      <span className={`${detail} ${deadlineClass}`}>
-        마감시간 : {deadline}
-      </span>
-      {/* <PhoneAuth
+    const {
+      foodDiv,
+      img,
+      detail,
+      menuClass,
+      placeClass,
+      addressClass,
+      prevPriceClass,
+      saledPriceClass,
+      deadlineClass,
+    } = styles;
+    return (
+      <div className={foodDiv}>
+        <GoToPrevPage />
+        <img className={`${img}`} alt="food-img" src={fileURL} />
+        <h3 className={`${detail} ${menuClass}`}>{menu}</h3>
+        <span className={`${detail} ${placeClass}`}>{place}</span>
+        <span className={`${detail} ${addressClass}`}>{address}</span>
+        <span className={`${detail} ${prevPriceClass}`}>
+          원가 : {Number(`${prevPrice}`).toLocaleString("en")}원
+        </span>
+        <span className={`${detail} ${saledPriceClass}`}>
+          할인가 : {Number(`${saledPrice}`).toLocaleString("en")}원
+        </span>
+        <span className={`${detail} ${deadlineClass}`}>
+          마감시간 : {deadline}
+        </span>
+        {/* <PhoneAuth
         id={id}
         menu={menu}
         place={place}
@@ -60,7 +60,7 @@ function Food() {
         createdAt={createdAt}
         fileURL={fileURL}
       /> */}
-      <ReservationForm
+        <ReservationForm
           id={id}
           menu={menu}
           place={place}
@@ -71,11 +71,11 @@ function Food() {
           createdAt={createdAt}
           fileURL={fileURL}
         />
-    </div>
-  );
-  // } catch (err) {
-  //   return <CompleteReserv />;
-  // }
+      </div>
+    );
+  } catch (err) {
+    return <CompleteReserv />;
+  }
 }
 
 export default Food;
